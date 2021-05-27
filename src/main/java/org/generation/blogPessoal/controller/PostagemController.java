2192f85fd6f3daeb167d22c1e0820b711e8f1c98
ismarps <ismarprobst@gmail.com>
2021-05-27 19:39:46 -0300

    Adicionado elementos da video aula do dia 27/05

--- src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -1,15 +1,19 @@
-package org.generation.blogPessoal.controller;
+	package org.generation.blogPessoal.controller;
 
 import java.util.List;
 
 import org.generation.blogPessoal.model.Postagem;
 import org.generation.blogPessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +39,24 @@ public class PostagemController {
 	@GetMapping("/titulo/{titulo}")
 	private ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo) {
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem) {  //objeto do tipo Postagem com o nome postagem
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+		
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> put (@RequestBody Postagem postagem) {  //objeto do tipo Postagem com o nome postagem
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+		
+		
 	}
 }
