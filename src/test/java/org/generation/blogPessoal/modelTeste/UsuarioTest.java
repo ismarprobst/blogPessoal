f4fb36d7ceacb4c21d2580ef56d4a401b0e767c5
ismarps <ismarprobst@gmail.com>
2021-06-11 16:31:49 -0300

    Feat Doc Swagger e 1o deploy

--- src/test/java/org/generation/blogPessoal/modelTeste/UsuarioTest.java
+++ src/test/java/org/generation/blogPessoal/modelTeste/UsuarioTest.java
@@ -16,29 +16,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@SpringBootTest (webEnvironment = WebEnvironment.RANDOM_PORT )
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class UsuarioTest {
 
 	public Usuario usuario; // instancia um objeto usuario
-	
+
 	@Autowired
-	private ValidatorFactory factory = Validation.buildDefaultValidatorFactory(); 
+	private ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
 	Validator validator = factory.getValidator();
-	
+
 	@BeforeEach
 	public void start() {
-		usuario = new Usuario("Ismar Santos", "ismarNinjao","batatinha123");
+		usuario = new Usuario("Ismar Santos", "ismarNinjao", "batatinha123");
 	}
-	
+
 	@Test
 	public void testValidationAtributos() {
-		
+
 		usuario.setNome("Ana");
 		usuario.setUsuario("ana91");
 		usuario.setSenha("123456");
-		
+
 		Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
 		System.out.println(violations.toString());
 		assertTrue(violations.isEmpty());
+	}
+
+	@Test
+	void testValidaAtributosNulos() {
+		Usuario usuarioErro = new Usuario();
+		usuarioErro.setUsuario("Yuri Boaz");
+		Set<ConstraintViolation<Usuario>> violacao = validator.validate(usuarioErro);
+		System.out.println(violacao.toString());
+		assertFalse(violacao.isEmpty());
 	}
 }
