aee1dcaf4f7a405f8db605f0000035ccbd6cbe36
ismarps <ismarprobst@gmail.com>
2021-07-07 17:46:25 -0300

    remocao das liberacoes de rota

--- src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
+++ src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
@@ -34,11 +34,10 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/**").permitAll()
+		
 		.antMatchers("/usuarios/logar").permitAll()
 		.antMatchers("/usuarios/cadastrar").permitAll()
-		.antMatchers(HttpMethod.GET ,"/postagens").permitAll()
-		.antMatchers(HttpMethod.GET ,"/tema").permitAll()
+		
 		.anyRequest().authenticated()
 		.and().httpBasic()
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -47,3 +46,9 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
 	}
 	
 }
+ /*
+.antMatchers("/**").permitAll() 
+.antMatchers(HttpMethod.GET ,"/postagens").permitAll()
+		.antMatchers(HttpMethod.GET ,"/tema").permitAll()
+
+*/
